3414b3b933f1aea3a5454ec8924c5c25a6610d95
jfietkau <julian@julian-fietkau.de>
2017-08-19 00:50:06 +0200

    populate focused password field (or empty ones) with the password

--- injector.js
+++ injector.js
@@ -1,3 +1,18 @@
+// This code is injected into the active tab when the extension is activated.
+
 function passwordshaker_fill(password) {
-  console.log("password has been injected: " + password);
+  if(typeof document.activeElement !== 'undefined' && document.activeElement !== null
+     && document.activeElement.tagName.toLowerCase() == "input" && document.activeElement.type == "password") {
+    // If a password field is focused, fill that one in.
+    document.activeElement.value = password;
+  } else {
+    // Otherwise, fill in all empty password fields on the page.
+    var allInputs = document.getElementsByTagName("input");
+    console.log(allInputs);
+    for(var i = 0; i < allInputs.length; i++) { 
+      if(allInputs[i].type == "password" && allInputs[i].value.length == 0) {
+        allInputs[i].value = password;
+      }
+    }
+  }
 }
