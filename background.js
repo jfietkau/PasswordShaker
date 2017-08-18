15ac2d1b7275a8de65926f37ce8e078d30c7a902
jfietkau <julian@julian-fietkau.de>
2017-08-18 19:25:34 +0200

    restrict context menu item to password fields

--- background.js
+++ background.js
@@ -9,7 +9,7 @@ browser.browserAction.onClicked.addListener(activateOnField);
 browser.contextMenus.create({
     id: "password-shaker",
     title: "PasswordShaker",
-    contexts: ["all"],
+    contexts: ["password"],
 });
 browser.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === "password-shaker") {
