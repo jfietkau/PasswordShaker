7376a921ca6ed57be92ff5946c3dc49c6238cf3b
jfietkau <julian@julian-fietkau.de>
2017-08-22 00:21:17 +0200

    add settings screen (not functional yet)

--- injector.js
+++ injector.js
@@ -8,7 +8,6 @@ function passwordshaker_fill(password) {
   } else {
     // Otherwise, fill in all empty password fields on the page.
     var allInputs = document.getElementsByTagName("input");
-    console.log(allInputs);
     for(var i = 0; i < allInputs.length; i++) { 
       if(allInputs[i].type == "password" && allInputs[i].value.length == 0) {
         allInputs[i].value = password;
