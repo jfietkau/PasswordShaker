5a250a825a4552839e125357587ab136024e65e8
jfietkau <julian@julian-fietkau.de>
2017-08-22 21:01:01 +0200

    populate profile settings from stored data

--- settings-ui.js
+++ settings-ui.js
@@ -1,7 +1,45 @@
 function populateSettingsForm(settings) {
   for(var property in settings) {
-    if(settings.hasOwnProperty(property)) {
+    if(property == "profiles") {
+      for(var i = 0; i < settings.profiles.length; i++) {
+        if(!document.getElementById("profileTab" + i)) {
+          var plusTab = document.getElementById("profileTabX"); 
+          var newRadio = document.createElement("input");
+          newRadio.type = "radio";
+          newRadio.name = "profileTabs";
+          newRadio.id = "profileTab" + i;
+          document.getElementById("profiles").insertBefore(newRadio, plusTab);
+          var newLabel = document.createElement("label");
+          newLabel.htmlFor = "profileTab" + i;
+          var tabTitle = settings.profiles[i].profileName;
+          if(tabTitle.length == 0) {
+            if(i == 0) {
+              tabTitle = "(default profile)";
+            } else {
+              tabTitle = "(profile " + i + ")";
+            }
+          }
+          var labelContent = document.createTextNode(tabTitle);
+          newLabel.appendChild(labelContent);
+          document.getElementById("profiles").insertBefore(newLabel, plusTab);          
+        }
+      }
+      var superfluousTabIndex = settings.profiles.length;
+      while(document.getElementById("profileTab" + superfluousTabIndex)) {
+        document.getElementById("profiles").removeChild(document.getElementById("profileTab" + superfluousTabIndex));
+      }
+      document.getElementById("profileTab0").checked = true;
+      populateProfileArea(settings.profiles[0]);
+    } else if(settings.hasOwnProperty(property)) {
       populateSettingsElement(property, settings[property]);
+    }
+  }
+}
+
+function populateProfileArea(profileSettings) {
+  for(var property in profileSettings) {
+    if(profileSettings.hasOwnProperty(property)) {
+      populateSettingsElement(property, profileSettings[property]);
     }
   }
 }
