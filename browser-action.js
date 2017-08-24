087d1dca30870a0613d83a1cfa0e34bbf0017902
jfietkau <julian@julian-fietkau.de>
2017-08-24 21:01:05 +0200

    show verification match icon in master password popup

--- browser-action.js
+++ browser-action.js
@@ -1,9 +1,12 @@
+function removeElementById(elemId) {
+  var elem = document.getElementById(elemId);
+  elem.parentNode.removeChild(elem);
+}
+
 function setupPopupForm(settings) {
-  //alert(settings.verifyMasterPassword);
   if(!settings.verifyMasterPassword) {
-    var confirmationElem = document.getElementById("masterPasswordConfirmation");
-    confirmationElem.value = "abc";
-    confirmationElem.parentNode.removeChild(confirmationElem);
+    removeElementById("masterPasswordConfirmation");
+    removeElementById("equalsIcon");
   }
   if(settings.showMasterPassword) {
     var passwordFields = document.getElementsByClassName("passwordEntry");
@@ -11,9 +14,32 @@ function setupPopupForm(settings) {
       passwordFields[i].type = "text";
     }
   }
+  if(!settings.storeMasterPasswordHash) {
+    removeElementById("matchStoredIcon");    
+  }
 }
 
 function updatePopupForm(settings) {
+  var noProblems = true;
+  if(settings.verifyMasterPassword) {
+    var entered = document.getElementById("masterPassword").value;
+    var enteredConf = document.getElementById("masterPasswordConfirmation").value;
+    var equalsIcon = document.getElementById("equalsIcon");
+    if(enteredConf.length == 0) {
+      equalsIcon.src = "/icons/equals-gray.svg";
+      equalsIcon.alt = "Master password confirmation not entered yet";
+      noProblems = false;
+    } else if(entered != enteredConf) {
+      equalsIcon.src = "/icons/equals-red.svg";
+      equalsIcon.alt = "Master passwords do not match!";
+      noProblems = false;
+    } else {
+      equalsIcon.src = "/icons/equals-green.svg";
+      equalsIcon.alt = "Master passwords match";
+    }
+    equalsIcon.title = equalsIcon.alt;
+  }
+  document.getElementById("okButton").disabled = !noProblems;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -21,4 +47,10 @@ document.addEventListener("DOMContentLoaded", () => {
     setupPopupForm(currentSettings);
     updatePopupForm(currentSettings);
   });
+  var passwordEntries = document.getElementsByClassName("passwordEntry");
+  for(var i = 0; i < passwordEntries.length; i++) {
+    passwordEntries[i].addEventListener("keyup", () => {
+      updatePopupForm(currentSettings);
+    });
+  }
 });
