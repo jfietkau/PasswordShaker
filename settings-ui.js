d75a03602cb9d65289c12a447493bb4607a94d06
jfietkau <julian@julian-fietkau.de>
2017-08-24 14:50:59 +0200

    remove settings save and load debug buttons

--- settings-ui.js
+++ settings-ui.js
@@ -208,8 +208,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  document.getElementById("loadButton").addEventListener("click", loadSettings);
-  document.getElementById("saveButton").addEventListener("click", saveSettings);
   document.getElementById("wipeButton").addEventListener("click", clearSettings);
   document.getElementById("showButton").addEventListener("click", debug_showSettings);
 });
