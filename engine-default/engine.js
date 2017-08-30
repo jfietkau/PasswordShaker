208d99dc194a05466049cb6f0ffd698b49a10b8c
jfietkau <julian@julian-fietkau.de>
2017-08-30 15:25:28 +0200

    add bcrypt support to default engine

--- engine-default/engine.js
+++ engine-default/engine.js
@@ -7,6 +7,34 @@ var str2arr = function(str) {
   return textEncoder.encode(str);
 }
 
+function base64ToArrayBuffer(b64str) {
+    b64str += "===";
+    b64str = b64str.slice(0, b64str.length - (b64str.length % 4));
+
+    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+    var result = new Array();
+
+    for(var i = 0; i < b64str.length; i += 4) {
+        var sub1 = chars.indexOf(b64str.charAt(i));
+        var sub2 = chars.indexOf(b64str.charAt(i + 1));
+        var sub3 = chars.indexOf(b64str.charAt(i + 2));
+        var sub4 = chars.indexOf(b64str.charAt(i + 3));
+
+        var bits = (sub1 << 18) | (sub2 << 12) | (sub3 << 6) | sub4;
+
+        result.push((bits >>> 16) & 0xff);
+
+        if(sub3 != 64) {
+          result.push((bits >>> 8) & 0xff);
+        }
+        if(sub4 != 64) {
+          result.push(bits & 0xff);
+        }
+    }
+
+    return Uint8Array.from(result);
+}
+
 function getRandomBytes(numberOfBytes) {
   var buffer = new ArrayBuffer(numberOfBytes);
   var uint8View = new Uint8Array(buffer);
@@ -92,6 +120,17 @@ function generatePassword(masterPassword, url, settings) {
     } else if(settings.hashAlgorithm.startsWith("pbkdf2-hmac-sha256-")) {
       var iterations = parseInt(settings.hashAlgorithm.slice("pbkdf2-hmac-sha256-".length));
       hashResult = asmCrypto.PBKDF2_HMAC_SHA256.hex(masterPassword + domain, thMainSalt, iterations, settings.passwordLength);
+    } else if(settings.hashAlgorithm.startsWith("bcrypt-")) {
+      var costFactor = parseInt(settings.hashAlgorithm.slice("bcrypt-".length));
+      var collapsedSalt = new Uint8Array(16);
+      for(var i = 0; i < toHash.length; i++) {
+        collapsedSalt[i % 16] = collapsedSalt[i % 16] ^ toHash[i];
+      }
+      var salt = "$2a$" + costFactor + "$";
+      salt += base64ArrayBuffer(collapsedSalt).replace(/\+/g, ".").slice(0, 22);
+      var rawHash = dcodeIO.bcrypt.hashSync(masterPassword, salt);
+      hashResult = rawHash.slice(salt.length);
+      hashResult = arr2hex(base64ToArrayBuffer(hashResult.replace(/\./g, "+")));
     }
     while(hashResult.length >= 8 && generatedPassword.length < settings.passwordLength) {
       var hashPart = parseInt(hashResult.slice(0, 8), 16) >>> 0;
