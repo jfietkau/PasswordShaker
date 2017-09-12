d4b0883e9ba67b38b20bf08c7e61217d5d7225e7
jfietkau <julian@julian-fietkau.de>
2017-09-12 15:10:54 +0200

    fire some keyboard events when filling in password fields

--- injector.js
+++ injector.js
@@ -1,21 +1,37 @@
 // This code is injected into the active tab as a content script.
 
+// Fills in one password field and dispatches some events to simulate key presses
+function fillPasswordField(passwordField, password) {
+  var event = new KeyboardEvent("keydown", {});
+  passwordField.dispatchEvent(event);
+  event = new KeyboardEvent("keypress", {});
+  passwordField.dispatchEvent(event);
+  event = new KeyboardEvent("keyup", {});
+  passwordField.dispatchEvent(event);
+  passwordField.value = password;
+  event = document.createEvent("HTMLEvents");
+  event.initEvent("change", false, true);
+  passwordField.dispatchEvent(event);
+}
+
+// Fills in either the currently focused password field or all password fields on the page
 function fillPassword(password) {
   if(typeof document.activeElement !== 'undefined' && document.activeElement !== null
      && document.activeElement.tagName.toLowerCase() == "input" && document.activeElement.type == "password") {
     // If a password field is focused, fill that one in.
-    document.activeElement.value = password;
+    fillPasswordField(document.activeElement, password);
   } else {
     // Otherwise, fill in all empty password fields on the page.
     var allInputs = document.getElementsByTagName("input");
     for(var i = 0; i < allInputs.length; i++) {
       if(allInputs[i].type == "password" && allInputs[i].value.length == 0) {
-        allInputs[i].value = password;
+        fillPasswordField(allInputs[i], password);
       }
     }
   }
 }
 
+// Installs a mutation listener that updates the backend if password fields are added or removed.
 function installChangeListener(tabIdPassthrough) {
   var observer = new MutationObserver(function(mutations) {
     sendNumberOfPasswordFields(tabIdPassthrough);
@@ -24,6 +40,7 @@ function installChangeListener(tabIdPassthrough) {
   observer.observe(document.getElementsByTagName("body")[0], config);
 }
 
+// Sends the current number of password fields on the page to the backend.
 function sendNumberOfPasswordFields(tabIdPassthrough) {
   var number = 0;
   var inputs = document.getElementsByTagName("input");
