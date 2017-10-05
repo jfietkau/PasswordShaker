7e53bb3c79f9da4b57d1ed6246add4446c306e95
jfietkau <julian@julian-fietkau.de>
2017-10-05 18:05:51 +0200

    add license header to first-party code files

--- browser-action.js
+++ browser-action.js
@@ -8,6 +8,29 @@ function removeElementById(elemId) {
   var elem = document.getElementById(elemId);
   elem.parentNode.removeChild(elem);
 }
+/**
+ * PasswordShaker
+ * https://github.com/jfietkau/PasswordShaker/
+ *
+ * Copyright (c) 2017 Julian Fietkau
+ *
+ *************************************************************************
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *************************************************************************
+ */
 
 function setupPopupForm(settings) {
   if(settings.profiles.length == 1) {
