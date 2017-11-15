c0d1b073cbf9ed3ddc2bec6550a440162ac7e85d
jfietkau <julian@julian-fietkau.de>
2017-11-15 19:46:44 +0100

    groundwork for selectable page action icon style

--- background.js
+++ background.js
@@ -309,7 +309,7 @@ function animatePageAction(script, tabId) {
   // Display the first frame of the given script
   var frame = script[0].split(":");
   browser.pageAction.setIcon({
-    path: "/icons/logo-" + frame[0] + ".svg",
+    path: "/icons/pageaction-default-" + frame[0] + ".svg",
     tabId: tabId
   });
   // Wait for the specified delay, then recurse to the rest of the script
@@ -564,6 +564,10 @@ browser.commands.onCommand.addListener(function(command) {
 function reactToTabChange(tabId, newUrl) {
   // Mainly what we do here is figure out if the page action icon should be displayed right now,
   // and act accordingly.
+  browser.pageAction.setIcon({
+    path: "/icons/pageaction-default-b.svg",
+    tabId: tabId
+  });
   loadSettings().then(() => {
     session.currentUrl = newUrl;
     session.currentProfile = null;
